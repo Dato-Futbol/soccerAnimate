051520fa850fbbd7758468c4098e7d431305dfca
Bustami <ismaelgomezs@gmail.com>
2023-03-10 22:09:09 -0500

    adding parameters long and width of the pitch

--- R/get_pitch.R
+++ R/get_pitch.R
@@ -2,6 +2,8 @@
 #'
 #' @param pitch_fill colour used to fill the pitch
 #' @param pitch_col colour used for lines of the pitch
+#' @param pitch_long long of the pitch in meters
+#' @param pitch_width width of the pitch in meters
 #'
 #' @return ggplot object
 #' @import ggplot2
@@ -11,7 +13,7 @@
 #' @keywords internal
 #'
 
-get_pitch <- function(pitch_fill = "#74a9cf", pitch_col = "lightgrey"){
+get_pitch <- function(pitch_fill = "#74a9cf", pitch_col = "lightgrey", pitch_long = 105, pitch_width = 68){
 
         ggplot() +
                 theme_bw() +
@@ -28,22 +30,22 @@ get_pitch <- function(pitch_fill = "#74a9cf", pitch_col = "lightgrey"){
                 scale_x_continuous(limits = c(-5, 110), expand = c(0, 0)) +
                 scale_y_continuous(limits = c(-5, 73), expand = c(0, 0)) +
                 #ggtitle("Soccer pitch", "with ggplot2") +
-                geom_rect(aes(xmin = 0, xmax = 105, ymin = 0, ymax = 68), fill = pitch_fill, col = pitch_col) +
+                geom_rect(aes(xmin = 0, xmax = pitch_long, ymin = 0, ymax = pitch_width), fill = pitch_fill, col = pitch_col) +
                 geom_rect(aes(xmin = 0, xmax = 16.5, ymin = 13.85, ymax = 54.15), fill = pitch_fill, col = pitch_col) +
-                geom_rect(aes(xmin = 88.5, xmax = 105, ymin = 13.85, ymax = 54.15), fill = pitch_fill, col = pitch_col) +
+                geom_rect(aes(xmin = 88.5, xmax = pitch_long, ymin = 13.85, ymax = 54.15), fill = pitch_fill, col = pitch_col) +
                 geom_rect(aes(xmin = 0, xmax = 5.5, ymin = 24.85, ymax = 43.15), fill = pitch_fill, col = pitch_col) +
-                geom_rect(aes(xmin = 99.5, xmax = 105, ymin = 24.85, ymax = 43.15), fill = pitch_fill, col = pitch_col) +
+                geom_rect(aes(xmin = 99.5, xmax = pitch_long, ymin = 24.85, ymax = 43.15), fill = pitch_fill, col = pitch_col) +
                 geom_rect(aes(xmin = -2, xmax = 0, ymin = 30.34, ymax = 37.66), fill = "transparent", col = pitch_col) +
-                geom_rect(aes(xmin = 105, xmax = 107, ymin = 30.34, ymax = 37.66), fill = "transparent", col = pitch_col) +
+                geom_rect(aes(xmin = pitch_long, xmax = 107, ymin = 30.34, ymax = 37.66), fill = "transparent", col = pitch_col) +
                 geom_circle(aes(x0 = 52.5, y0 = 34, r = 9.15), fill = pitch_fill, col = pitch_col) +
-                geom_segment(aes(x = 52.5, xend = 52.5, y = 0, yend = 68), col = pitch_col) +
+                geom_segment(aes(x = 52.5, xend = 52.5, y = 0, yend = pitch_width), col = pitch_col) +
                 geom_point(aes(x = 11, y = 34), col = pitch_col) +
                 geom_point(aes(x = 94, y = 34), col = pitch_col) +
                 geom_point(aes(x = 52.5, y = 34), col = pitch_col) +
                 geom_arc(aes(x0 = 11, y0 = 34, r = 9.15, start = 37*pi/180, end = 143*pi/180), col = pitch_col) +
                 geom_arc(aes(x0 = 94, y0 = 34, r = 9.15, start = -37*pi/180, end = -143*pi/180), col = pitch_col) +
                 geom_arc(aes(x0 = 0, y0 = 0, r = 1, start = 0, end = 90*pi/180), col = pitch_col) +
-                geom_arc(aes(x0 = 0, y0 = 68, r = 1, start = 90*pi/180, end = 180*pi/180), col = pitch_col) +
-                geom_arc(aes(x0 = 105, y0 = 68, r = 1, start = 180*pi/180, end = 270*pi/180), col = pitch_col) +
-                geom_arc(aes(x0 = 105, y0 = 0, r = 1, start = 270*pi/180, end = 360*pi/180), col = pitch_col)
+                geom_arc(aes(x0 = 0, y0 = pitch_width, r = 1, start = 90*pi/180, end = 180*pi/180), col = pitch_col) +
+                geom_arc(aes(x0 = pitch_long, y0 = pitch_width, r = 1, start = 180*pi/180, end = 270*pi/180), col = pitch_col) +
+                geom_arc(aes(x0 = pitch_long, y0 = 0, r = 1, start = 270*pi/180, end = 360*pi/180), col = pitch_col)
 }
